af40dee1cbac20a03c8019ae19bf6a0b1b61d030
Colm Morgan <colm.a.morgan@gmail.com>
2012-03-27 04:56:39 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Plusev::Application.config.secret_token = '9d7767a8639098204023dfb9a9cd3b31313d428ad5afb00bf71962a27be1dbd018b4e37db8618a71dad1d2f8c21b455312e96835aa7ddc431e1860e799a6d295'
